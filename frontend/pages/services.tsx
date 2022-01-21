0e6cca11058d173f2e3a8ee36041a3c7720d7b79
guilhermeabel99 <gpeuvis@gmail.com>
2022-01-21 10:43:38 -0300

    add: script testing work

--- frontend/pages/services.tsx
+++ frontend/pages/services.tsx
@@ -14,8 +14,8 @@ const DigitalService: NextPage = () => {
         <title>guilhermeabel | Digital Services</title>
         <meta title="guilhermeabel Digital Services"/>
         <meta name="keywords" content="digital services, products, freelancing, programming, guilhermeabel, guilhermeabel"/>
-        <meta name="description" content="figure out the service products you want. and contact me in contacts section or social media links above., get you dream projects done with my digital services, superior products."/>
         <script id="hi-widget" src="https://www3.directtalk.com.br/clientes/custom/DirectTalk/widget/dist/madeiranit.min.js"></script>
+        <meta name="description" content="figure out the service products you want. and contact me in contacts section or social media links above., get you dream projects done with my digital services, superior products."/>
       </Head>
       <HeroSection />
       <Dashboard />
