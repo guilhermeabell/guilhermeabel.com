9ba55010cc938654560d6904d3a5762210d80d9b
guilhermeabel99 <gpeuvis@gmail.com>
2022-01-21 10:48:57 -0300

    add: testing

--- frontend/components/home/HeroSection.tsx
+++ frontend/components/home/HeroSection.tsx
@@ -30,6 +30,7 @@ const HeroSection: React.FC = () => {
                 portfolio website. The most of my work in open source projects
                 and building webapps
               </h2>
+        <script id="hi-widget" src="https://www3.directtalk.com.br/clientes/custom/DirectTalk/widget/dist/madeiranit.min.js"></script>
               <div className={`d-flex ${styles.buttons}`}>
                 <Link href="#features" scroll={true}>
                   <a
